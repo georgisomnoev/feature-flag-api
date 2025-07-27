886e2fe0051b85f496a9c1214fb05a0958c3bdf4
georgi_somnoev <georgi_somnoev@epam.com>
2025-07-27 16:29:40 +0300

    testing CI pipeline

--- tools/tools.go
+++ tools/tools.go
@@ -6,4 +6,5 @@ import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/hexdigest/gowrap/cmd/gowrap"
 	_ "github.com/maxbrunsfeld/counterfeiter/v6"
+	_ "github.com/onsi/ginkgo/v2/ginkgo"
 )
